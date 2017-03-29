654e9452dbfae371df3b4d2d3aadf4c5deb7d28d
jason <kwakjason94@mgmail.com>
2017-03-29 10:29:22 -0700

    Added IEEE API whitebox test

---
+++ test/whitebox/getSearchResultTest.php
@@ -0,0 +1,59 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class IEEESearchResultsTest extends TestCase
+{
+	/*
+	 * Get IEEE json response
+	 */
+	public function provider()
+	{
+		return 1;
+	}
+
+	/*
+	 * @dataProvider provider
+	 */
+	public function testJSONGivesXResults()
+	{
+		$this->assertTrue(true);
+		return 1;
+	}
+
+	/*
+	 * @dataProvider provider
+	 */
+	public function testJSONGivesCorrectAuthor()
+	{
+		$this->assertTrue(true);
+		return 1;
+	}
+
+	/*
+	 * @dataProvider provider
+	 */
+	public function testJSONGivesCorrectDocument()
+	{
+		$this->assertTrue(true);
+		return 1;
+	}
+
+	/*
+	 * @dataProvider provider
+	 */
+	public function testJSONGivesCorrectWordsOnDocument()
+	{
+		$this->assertTrue(true);
+		return 1;
+	}
+
+	/*
+	 * @dataProvider provider
+	 */
+	public function testJSONGivesCorrectbibtext()
+	{
+		$this->assertTrue(true);
+		return 1;
+	}
+}
+?>
