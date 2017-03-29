704347f7ce897d47f0e833965fd00d3d7075ce2d
Quinn Ellis <quinnellis@guest-wireless-upc-1601-10-120-031-125.usc.edu>
2017-03-28 19:48:20 -0700

    Small edits to the whitebox testing file mainTest.php

--- test/whitebox/mainTest.php
+++ test/whitebox/mainTest.php
@@ -3,11 +3,6 @@
 
 use PHPUnit\Framework\TestCase;
 
-
-function fromString() {
-    return (1); // Returns 0 for odd and 1 for even
-}
-
 function getAuthor() {
     return (1);
 }
@@ -29,12 +24,6 @@ function getFrequency() {
  */
 final class EmailTest extends TestCase
 {
-    //
-    public function testCanBeUsedAsString()
-    {
-        $this->assertEquals(2, fromString("hello")); 
-    }
-
     public function checkAuthor() {
         $this->assertEquals("", getAuthor("name"));
     }
@@ -45,10 +34,6 @@ final class EmailTest extends TestCase
 
     public function checkLink() {
         $this->assertEquals("", getLink("title"));
-    }
-
-    public function checkWordFrequency() {
-        $this->assertEquals("", getFrequency(50));
     }
 
     public function checkWordFrequency() {
