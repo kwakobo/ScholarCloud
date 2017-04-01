f7fc5eae764377de8180603ca2d48b481315cf1d
Jeffrey Nagel <jeffrean@usc.edu>
2017-04-01 14:24:25 -0700

    more tests added for ACMData parser

--- test/whitebox/parsertests.php
+++ test/whitebox/parsertests.php
@@ -4,13 +4,33 @@ use PHPUnit\Framework\TestCase;
 
 class ParserTest extends TestCase
 {
-    public function testDocumentParser()
+    public function testDocumentParserWithTEXT()
     {
-        $arr = array(array('publication_type' => 'PDF','authors' => '2', 'title' => '1', 'article' => 'http://dl.acm.org/ft_gateway.cfm?id=1101935&ftid=338180&dwn=1&#URLTOKEN#', 'bibtex' => '4'));
+        $arr = array(array('publication_type' => 'TEXT','authors' => 'authors', 'title' => 'title', 'article' => 'testdocument.txt', 'bibtex' => 'bibtex'));
         $dp = new DocumentParser($arr);
-        $text = file_get_contents("testdocument.txt");
+        $text = file_get_contents("testdocument.txt"); //expected text result
         $this->assertEquals(
-        	new Document("1","2","http://dl.acm.org/ft_gateway.cfm?id=1101935&ftid=338180&dwn=1&#URLTOKEN#","4",$text), 
+        	new Document("title","authors","testdocument.txt","bibtex",$text), 
+        	$dp->parseDocuments()[0]);
+    }
+
+    public function testDocumentParserWithPDF()
+    {
+        $arr = array(array('publication_type' => 'PDF','authors' => 'authors', 'title' => 'title', 'article' => 'testdocument.txt', 'bibtex' => 'bibtex'));
+        $dp = new DocumentParser($arr);
+        $text = file_get_contents("testdocument.txt"); //expected text result
+        $this->assertEquals(
+        	new Document("title","authors","http://dl.acm.org/ft_gateway.cfm?id=1101935&ftid=338180&dwn=1&#URLTOKEN#","bibtex",$text), //only works on USC wifi!!!
+        	$dp->parseDocuments()[0]);
+    }
+
+    public function testDocumentParserWithHTML()
+    {
+        $arr = array(array('publication_type' => 'HTML','authors' => 'authors', 'title' => 'title', 'article' => 'testdocument.txt', 'bibtex' => 'bibtex'));
+        $dp = new DocumentParser($arr);
+        $text = file_get_contents("testdocument.txt"); //expected text result
+        $this->assertEquals(
+        	new Document("title","authors",".html","bibtex",$text), 
         	$dp->parseDocuments()[0]);
     }
 
