b7084315c232b47d7cb96ca413635196b473cd54
jason <kwakjason94@mgmail.com>
2017-04-03 03:06:36 -0700

    Implemented IEEE whitebox testing

--- test/whitebox/getSearchResultTest.php
+++ test/whitebox/getSearchResultTest.php
@@ -1,77 +1,47 @@
 <?php
-use PHPUnit\Framework\TestCase;
+	use PHPUnit\Framework\TestCase;
+	require './../../src/db/ieee_api.php';
 
-class IEEESearchResultsTest extends TestCase
-{
-	/*
-	 * Get IEEE json response
-	 */
-	public function provider()
+	class IEEESearchResultsTest extends TestCase
 	{
-		return 1;
-	}
+		public function testJSONGivesXResults()
+		{
+			$ieee_resp = get("halfond", 1);
+			$this->assertTrue(count($ieee_resp) == 1);
 
-	/*
-	 * @dataProvider provider
-	 */
-	public function testJSONGivesXResults()
-	{
-		$this->assertTrue(true);
-		return 1;
-	}
+			$ieee_resp = get("halfond", 10);
+			$this->assertTrue(count($ieee_resp) == 10);
+		}
 
-	/*
-	 * @dataProvider provider
-	 */
-	public function testJSONGivesCorrectAuthor()
-	{
-		$this->assertTrue(true);
-		return 1;
-	}
+		public function testJSONGivesCorrectAuthor()
+		{
+			$ieee_resp = get("halfond", 2);
+			$this->assertTrue(strcmp($ieee_resp[0]["authors"], "Abdulmajeed Alameer;  Sonal Mahajan;  William G. J. Halfond") == 0);
+			$this->assertTrue(strcmp($ieee_resp[1]["authors"], "Abdulmajeed Alameer;  William G. J. Halfond") == 0);
+		}
 
-	/*
-	 * @dataProvider provider
-	 */
-	public function testJSONGivesCorrectDocument()
-	{
-		$this->assertTrue(true);
-		return 1;
-	}
+		public function testJSONGivesCorrectDocument()
+		{
+			$ieee_resp = get("halfond", 2);
+			$this->assertTrue(trim($ieee_resp[0]["article"]) == trim("http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7515472"));
+			$this->assertTrue(trim($ieee_resp[1]["article"]) == trim("http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7816457"));
+		}
 
-	/*
-	 * @dataProvider provider
-	 */
-	public function testJSONGivesCorrectWordsOnDocument()
-	{
-		$this->assertTrue(true);
-		return 1;
-	}
+		public function testJSONGivesCorrectbibtext()
+		{
+			$ieee_resp = get("halfond", 2);
+			/*$this->expectOutputString('http://ieeexplore.ieee.org/xpl/downloadCitations?citations-format=citation-only&download-format=download-bibtex&x=0&y=0&recordIds=7515472\n' +
+									'http://ieeexplore.ieee.org/xpl/downloadCitations?citations-format=citation-only&download-format=download-bibtex&x=0&y=0&recordIds=7816457');
+			print $ieee_resp[0]["bibtex"];
+			print $ieee_resp[1]["bibtex"];*/
+			$this->assertTrue(trim($ieee_resp[0]["bibtex"]) == trim("http://ieeexplore.ieee.org/xpl/downloadCitations?citations-format=citation-only&download-format=download-bibtex&x=0&y=0&recordIds=7515472"));
+			$this->assertTrue(trim($ieee_resp[1]["bibtex"]) == trim("http://ieeexplore.ieee.org/xpl/downloadCitations?citations-format=citation-only&download-format=download-bibtex&x=0&y=0&recordIds=7816457"));
+		}
 
-	/*
-	 * @dataProvider provider
-	 */
-	public function testJSONGivesCorrectbibtext()
-	{
-		$this->assertTrue(true);
-		return 1;
+		public function testReturnedResultIsSmallerThanSpecifiedTopNumSearch()
+		{
+			$ieee_resp = get("halfond", 100);
+			$this->assertEquals(count($ieee_resp), 18);
+		}
 	}
-	
-	/*
-	 * @dataProvider provider
-	 */
-	public function testCorrectCreateArticleArray()
-	{
-		$this->assertTrue(true);
-		return 1;
-	}
-	
-	/*
-	 * @dataProvider provider
-	 */
-	public function testReturnedResultIsSmallerThanSpecifiedTopNumSearch()
-	{
-		$this->assertTrue(true);
-		return 1;
-	}
-}
 ?>
