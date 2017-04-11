662cc9bfd501daa5f7e3fef84728c867010bf40b
jason <kwakjason94@mgmail.com>
2017-04-11 01:30:26 -0700

    Updated ACM_ParserTest.php test case for new API

--- test/whitebox/ACM_ParserTest.php
+++ test/whitebox/ACM_ParserTest.php
@@ -1,48 +1,36 @@
 <?php
-use PHPUnit\Framework\TestCase;
-require './../../src/db/ACM_Parser.php';
+	use PHPUnit\Framework\TestCase;
+	require './../../src/db/acm_api.php';
 
-final class ACMParserTest extends TestCase
-{
-    public function testParse()
-    {
-      $parser = new ACMParser();
-      $output = [];
-      $output[0]['authors'] = "Bruce Davie";
-      $output[0]['title'] = "Deployment experience with differentiated services";
-      $output[0]['article'] = "http://dl.acm.org/ft_gateway.cfm?id=944598&ftid=232772&dwn=1";
-      $output[0]['publication_type'] = "PDF";
-      $output[0]['bibtex'] = "http://dl.acm.org/downformats.cfm?id=944598&parent_id=&expformat=bibtex&CFID=746525198&CFTOKEN=27921863";
+	final class ACMParserTest extends TestCase
+	{
+		public function testParse()
+		{
+			$parser = new ACM_API();
+			$output = [];
+			$output[0]['authors'] = "William G. J. Halfond";
+			$output[0]['title'] = "Web application modeling for testing and analysis";
+			$output[0]['article'] = "http://dl.acm.org/ft_gateway.cfm?id=1496657";
+			$output[0]['publication_type'] = "PDF";
+			$output[0]['bibtex'] = "http://dl.acm.org/exportformats.cfm?expformat=bibtex&id=1496657";
 
-      $parse = $parser->parse("David", 1);
+			$parse = $parser->get_acm("halfond", 1);
 
-      $this->assertTrue(trim($parse[0]['authors']) == trim($output[0]['authors']));
-      $this->assertTrue(trim($parse[0]['title']) == trim($output[0]['title']));
-      $this->assertTrue(trim($parse[0]['article']) == trim($output[0]['article']));
-      $this->assertTrue(trim($parse[0]['publication_type']) == trim($output[0]['publication_type']));
-      $this->assertTrue(trim(substr($parse[0]['bibtex'],0,strpos(strtolower($parse[0]['bibtex']), '?id='))) == trim(substr($output[0]['bibtex'],0,strpos(strtolower($output[0]['bibtex']), '?id='))));
+			$this->assertTrue(trim($parse[0]['authors']) == trim($output[0]['authors']));
+			$this->assertTrue(trim($parse[0]['title']) == trim($output[0]['title']));
+			$this->assertTrue(trim($parse[0]['article']) == trim($output[0]['article']));
+			$this->assertTrue(trim($parse[0]['publication_type']) == trim($output[0]['publication_type']));
+			$this->assertTrue(trim(substr($parse[0]['bibtex'],0,strpos(strtolower($parse[0]['bibtex']), '?id='))) == trim(substr($output[0]['bibtex'],0,strpos(strtolower($output[0]['bibtex']), '?id='))));
+		}
 
+		public function testACMDatabaseGetsAbsctract()
+		{
 
-      $output = [];
-      $output[0]['authors'] = "Bruce Davie";
-      $output[0]['title'] = "Deployment experience with differentiated services";
-      $output[0]['article'] = "http://dl.acm.org/ft_gateway.cfm?id=944598&ftid=232772&dwn=1";
-      $output[0]['publication_type'] = "PDF";
-      $output[0]['bibtex'] = "http://dl.acm.org/downformats.cfm?id=944598&parent_id=&expformat=bibtex&CFID=746524322&CFTOKEN=64073858";
+		}
 
-      $output[1]['authors'] = "Bruce Davie";
-      $output[1]['title'] = "SIGCOMM: a view from the chair";
-      $output[1]['article'] = "http://dl.acm.org/ft_gateway.cfm?id=1629612&ftid=608078&dwn=1";
-      $output[1]['publication_type'] = "PDF";
-      $output[1]['bibtex'] = "http://dl.acm.org/downformats.cfm?id=1629612&parent_id=&expformat=bibtex&CFID=746524322&CFTOKEN=64073858";
-
-
-      $parse = $parser->parse("David", 2);
-      $this->assertTrue(trim($parse[1]['authors']) == trim($output[1]['authors']));
-      $this->assertTrue(trim($parse[1]['title']) == trim($output[1]['title']));
-      $this->assertTrue(trim($parse[1]['article']) == trim($output[1]['article']));
-      $this->assertTrue(trim($parse[1]['publication_type']) == trim($output[1]['publication_type']));
-      $this->assertTrue(trim(substr($parse[1]['bibtex'],0,strpos(strtolower($parse[1]['bibtex']), '?id='))) == trim(substr($output[1]['bibtex'],0,strpos(strtolower($output[1]['bibtex']), '?id='))));
-    }
-}
+		public function testACMDatabaseGetsConferenceName()
+		{
+			
+		}
+	}
 ?>
