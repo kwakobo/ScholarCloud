f5129df25b0a92254a6de690078f3785a7a170a5
jason <kwakjason94@mgmail.com>
2017-04-16 16:40:13 -0700

    Added subset article test case

---
+++ test/whitebox/testSearchSubsetList.php
@@ -0,0 +1,12 @@
+<?php
+	use PHPUnit\Framework\TestCase;
+	require './../../src/db/subsetsearch.php';
+
+	class SearchSublsetListTest extends TestCase
+	{
+		public function testSubset()
+		{
+			$this->assertTrue(true);
+		}
+	}
+?>
